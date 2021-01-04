a5ee2c0260838d93ffcb07ac0e3e246d3c8f0093
bengio salomon <sa-ben93@outlook.com>
2021-01-04 14:40:47 +0100

    ajout write

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-        }
+            string salut = "helloWorld";
+            Console.WriteLine(salut);
+            Console.Read();
+        }   
     }
 }
