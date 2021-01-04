6f98f3e8dcfba43fed88c47796d966bf3b19c4a9
bengio salomon <sa-ben93@outlook.com>
2021-01-04 16:07:29 +0100

    ajout date

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace HelloWorld
 {
@@ -10,10 +11,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            string salut = "helloWorld";
-            Console.WriteLine(salut);
-            Console.WriteLine($"{salut}");
-            Console.WriteLine(salut.Length);
+            string Salut = "helloWorld";
+            DateTime dateActuelle = DateTime.Now;
+            Console.WriteLine(dateActuelle.ToString("r", CultureInfo.CreateSpecificCulture("fr-FR")));
+            Console.WriteLine(Salut);
+            Console.WriteLine(dateActuelle);
+            Console.WriteLine($"{Salut}");
+            Console.WriteLine(Salut.Length);
             Console.Read();
         }   
     }
